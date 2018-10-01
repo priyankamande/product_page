6f801fadc759b86de47132a7e45beb1407163a1b
priyankamande <43761546+priyankamande@users.noreply.github.com>
2018-10-02 00:42:36 +0530

    MAin JS File

---
+++ menu.js
@@ -0,0 +1,72 @@
+// Mobile view header
+$(document).ready(function(){
+
+    $("#burger-nav").on("click", function(){
+    
+        $("header nav ul").toggleClass("open");
+        
+    });
+
+});
+
+//Scrollable page content
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
+
+//Color Selector
+$('.color').on('click', function() {
+   $(this).addClass('selected').siblings().removeClass('selected');
+    var hex = $(this).css("background-color");
+    var hexVal = $('.active-1').attr("value");
+    var styles = {
+      backgroundColor : hex
+    };
+});
+
+//Related Product Slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("productSlides");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
